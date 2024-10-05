002ede9c5ba1ecbe3699ab1c23aca839c7a1a417
Solomon Nwante <myphilothea@gmail.com>
2024-10-05 16:59:02 +0100

    refactor: change code implementaion

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((profile) => {
-      const [photo, user] = profile;
+    .then(([photo, user]) => {
       console.log(photo.body, user.firstName, user.lastName);
     })
-    .catch(() => new Error('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
