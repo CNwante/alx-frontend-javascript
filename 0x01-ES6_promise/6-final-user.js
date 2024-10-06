a9e476b3a91d78b1d995af8e4c5bd9ea049d3372
Solomon Nwante <myphilothea@gmail.com>
2024-10-06 01:05:00 +0100

    Fix requirment issues resulting to errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,23 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const output = []; // Create an empty array for results
-
-      for (const item of results) { // Use a for loop to iterate
-        if (item.status === 'fulfilled') {
-          output.push({
-            status: item.status,
-            value: item.value,
-          });
-        } else {
-          output.push({
-            status: item.status,
-            value: item.reason,
-          });
-        }
-      }
-
-      return output; // Return the results array
-    });
+    .then((results) =>
+      results.map((item) => ({
+        status: item.status,
+        value: item.status === 'fulfilled' ? item.value : String(item.reason),
+      }))
+    );
 }
