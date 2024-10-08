0ef04a592f73345898e297a0fe252f0267700b8c
Solomon Nwante <myphilothea@gmail.com>
2024-10-08 19:45:46 +0100

    Submit task 4: update students grades by city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+import getStudentsByLocation from './2-get_students_by_loc';
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsInCity = getStudentsByLocation(students, city);
+  return studentsInCity.map((student) => {
+    const studentGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+
+    return {
+      ...student,
+      grade: studentGrade ? studentGrade.grade : 'N/A',
+    };
+  });
+}
