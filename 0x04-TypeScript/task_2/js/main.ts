5dc881f1d7a08b06c514dc8551fa5caac7fd70b5
Solomon Nwante <myphilothea@gmail.com>
2024-10-11 16:49:29 +0100

    Update recompiled changes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -71,3 +71,14 @@ function executeWork(employee: Director | Teacher): void {
 // Test the implementation
 executeWork(createEmployee(200)); // Should print: Getting to work
 executeWork(createEmployee(1000)); // Should print: Getting to director tasks
+
+
+// Task 7. String literal types
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  return `Teaching ${todayClass}`;
+}
+
+console.log(teachClass('Math'));      // Outputs: Teaching Math
+console.log(teachClass('History'));   // Outputs: Teaching History
