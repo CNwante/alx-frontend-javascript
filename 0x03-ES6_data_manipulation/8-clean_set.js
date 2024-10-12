a7c514c52d7a45a815a02311aea184edaf1469d8
Solomon Nwante <myphilothea@gmail.com>
2024-10-12 05:58:43 +0100

    Update task 8 solution

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString || startString.length === 0) return ''; // Handle empty startString
+  if (!startString || startString.length === 0) return '';
 
   let resultString = '';
 
   for (const value of set) {
     if (value.startsWith(startString)) {
-      // Slice off the startString and append the rest
       const restOfString = value.slice(startString.length);
       if (restOfString) {
         resultString += resultString ? `-${restOfString}` : restOfString;
